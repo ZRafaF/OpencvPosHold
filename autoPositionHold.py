e850b1456be53f67e9e9c2191902356edc8b03bf
linuxvm <rafaelmeneses@alunos.utfpr.edu.br>
2022-12-05 12:11:14 -0300

    Started working on the main program autoPositionHold.py

---
+++ autoPositionHold.py
@@ -0,0 +1,155 @@
+import cv2 as cv
+from cv2 import aruco
+import numpy as np
+import os
+import math
+
+calib_data_path = "calib_data/MultiMatrix.npz"
+
+calib_data = np.load(calib_data_path)
+print(calib_data.files)
+
+cam_mat = calib_data["camMatrix"]
+dist_coef = calib_data["distCoef"]
+r_vectors = calib_data["rVector"]
+t_vectors = calib_data["tVector"]
+
+MARKER_SIZE = 15  # centimeters
+
+marker_dict = aruco.Dictionary_get(aruco.DICT_4X4_100)
+
+param_markers = aruco.DetectorParameters_create()
+
+cap = cv.VideoCapture(0)
+have_display = bool(os.environ.get("DISPLAY", None))
+
+
+## comprimindo a captura
+cap.set(3, 400)
+cap.set(4, 300)
+cap.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc("M", "J", "P", "G"))
+
+
+def get_quaternion_from_euler(pitch, yaw, roll):
+    """
+    Convert an Euler angle to a quaternion.
+
+    Input
+      :param roll: The roll (rotation around x-axis) angle in radians.
+      :param pitch: The pitch (rotation around y-axis) angle in radians.
+      :param yaw: The yaw (rotation around z-axis) angle in radians.
+
+    Output
+      :return qx, qy, qz, qw: The orientation in quaternion [x,y,z,w] format
+    """
+    qx = np.sin(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) - np.cos(
+        roll / 2
+    ) * np.sin(pitch / 2) * np.sin(yaw / 2)
+    qy = np.cos(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2) + np.sin(
+        roll / 2
+    ) * np.cos(pitch / 2) * np.sin(yaw / 2)
+    qz = np.cos(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2) - np.sin(
+        roll / 2
+    ) * np.sin(pitch / 2) * np.cos(yaw / 2)
+    qw = np.cos(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) + np.sin(
+        roll / 2
+    ) * np.sin(pitch / 2) * np.sin(yaw / 2)
+
+    return [qx, qy, qz, qw]
+
+
+# Ganho nos eixos (menor = mais abrupto)
+ganhoX = 5
+ganhoY = 5
+
+# Qual o ID do marker objetivo
+targetId = 72
+
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    marker_corners, marker_IDs, reject = aruco.detectMarkers(
+        gray_frame, marker_dict, parameters=param_markers
+    )
+    if marker_corners:
+        rVec, tVec, mark = aruco.estimatePoseSingleMarkers(
+            marker_corners, MARKER_SIZE, cam_mat, dist_coef
+        )
+
+        # Calculando a rotação
+
+        # resolvendo PnP muito pesado
+        # _, rVecs, tVecs = cv.solvePnP(mark, marker_corners[0], cam_mat, dist_coef)
+
+        # -X o drone deve se mover para direita
+        # +X o drone deve se mover para frente
+        # -Y o drone deve se mover para frente
+        # +Y o drone deve se mover para trás
+
+        # A camera está rotacionada 90° no sentido horário
+
+        total_markers = range(0, marker_IDs.size)
+        for ids, corners, i in zip(marker_IDs, marker_corners, total_markers):
+            if ids != targetId:
+                continue
+            rotation = math.degrees(rVec[i][0][1]) + 180
+
+            # Calculating the distance
+            distance = np.sqrt(
+                tVec[i][0][2] ** 2 + tVec[i][0][0] ** 2 + tVec[i][0][1] ** 2
+            )
+            deltaX = tVec[i][0][0]
+            deltaY = tVec[i][0][1]
+            deltaR = rotation
+
+            if have_display:
+                cv.polylines(
+                    frame,
+                    [corners.astype(np.int32)],
+                    True,
+                    (0, 255, 255),
+                    4,
+                    cv.LINE_AA,
+                )
+                corners = corners.reshape(4, 2)
+                corners = corners.astype(int)
+                top_right = corners[0].ravel()
+                top_left = corners[1].ravel()
+                bottom_right = corners[2].ravel()
+                bottom_left = corners[3].ravel()
+                # Draw the pose of the marker
+                point = cv.drawFrameAxes(
+                    frame, cam_mat, dist_coef, rVec[i], tVec[i], 4, 4
+                )
+                cv.putText(
+                    frame,
+                    f"id: {ids[0]} Dist: {round(distance, 2)}",
+                    top_right,
+                    cv.FONT_HERSHEY_PLAIN,
+                    1.3,
+                    (0, 0, 255),
+                    2,
+                    cv.LINE_AA,
+                )
+                cv.putText(
+                    frame,
+                    f"x:{round(tVec[i][0][0],1)} y: {round(tVec[i][0][1],1)} r:{round(rotation)}",
+                    bottom_right,
+                    cv.FONT_HERSHEY_PLAIN,
+                    1.0,
+                    (0, 0, 255),
+                    2,
+                    cv.LINE_AA,
+                )
+
+            print(f"x = {deltaX}, y = {deltaY}, r = {deltaR}")
+    if have_display:
+        cv.imshow("frame", frame)
+    key = cv.waitKey(1)
+    if key == ord("q"):
+        break
+cap.release()
+cv.destroyAllWindows()
