e9df3d61198f82f28af16609c33f79f024b85466
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2022-12-15 18:39:59 -0300

    created a new class

--- 2aprilTagDetect.py
+++ 2aprilTagDetect.py
@@ -1,25 +1,30 @@
-import cv2
+from __future__ import print_function
+
 import apriltag
 
 import numpy as np
 from argparse import ArgumentParser
+
+from imutils.video.pivideostream import PiVideoStream
+from imutils.video import FPS
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-from threading import Thread
-
-
-
+import argparse
+import imutils
 import time
+import cv2
 
-cap = cv2.VideoCapture(0)
+
+
+#cap = cv2.VideoCapture(0)
 CAP_WIDTH = 640
 CAP_HEIGHT = 480
 
 MARKER_SIZE = 15  # centimeters
 
 ## comprimindo a captura
-cap.set(3, CAP_WIDTH)
-cap.set(4, CAP_HEIGHT)
+#cap.set(3, CAP_WIDTH)
+#cap.set(4, CAP_HEIGHT)
 
 
 parser = ArgumentParser()
@@ -40,44 +45,7 @@ have_display = parser.parse_args().display
 
 print(f"ehSimulacao: {ehSimulacao}, recordCamera: {recordCamera}, have_display: {have_display}")
 
-class PiVideoStream:
-    def __init__(self, resolution=(320, 240), framerate=32):
-        # initialize the camera and stream
-        self.camera = PiCamera()
-        self.camera.resolution = resolution
-        self.camera.framerate = framerate
-        self.rawCapture = PiRGBArray(self.camera, size=resolution)
-        self.stream = self.camera.capture_continuous(self.rawCapture,
-            format="bgr", use_video_port=True)
-        # initialize the frame and the variable used to indicate
-        # if the thread should be stopped
-        self.frame = None
-        self.stopped = False
-    
-    def start(self):
-        # start the thread to read frames from the video stream
-        Thread(target=self.update, args=()).start()
-        return self
 
-    def update(self):
-        # keep looping infinitely until the thread is stopped
-        for f in self.stream:
-            # grab the frame from the stream and clear the stream in
-            # preparation for the next frame
-            self.frame = f.array
-            self.rawCapture.truncate(0)
-            # if the thread indicator variable is set, stop the thread
-            # and resource camera resources
-            if self.stopped:
-                self.stream.close()
-                self.rawCapture.close()
-                self.camera.close()
-                return
-    def read(self):
-        return self.frame
-    
-    def stop(self):
-        self.stopped = True
     
 
 # Parametros gerados do script aprilCalibrateCam.py
@@ -166,10 +134,17 @@ stream = camera.capture_continuous(
     rawCapture, 
     format="bgr",
 	use_video_port=True)
+    
+vs = PiVideoStream().start()
+time.sleep(1.0)
+fps = FPS().start()
+
 
 while True:
-    frame = stream.array
-    rawCapture.truncate(0)
+    frame = vs.read()
+    frame = imutils.resize(frame, width=CAP_WIDTH)
+
+    
     #ret, frame = cap.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
